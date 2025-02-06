fa027a9a356a2c06c600a4d0d1cbb1e7a952db1a
SauloGP <sauloproetti@gmail.com>
2025-02-05 21:26:51 -0300

    add idle animation for player

--- game/player/player.gd
+++ game/player/player.gd
@@ -2,8 +2,13 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta: float):
+func _physics_process(_delta: float):
 	_moviment()
+	_animation()
+
+func _animation():
+	$AnimatedSprite.play("idle")
+	pass
 
 func _moviment():
 	var direction = Input.get_vector("ui_left", "ui_right","ui_up", "ui_down")
