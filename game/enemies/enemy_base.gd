9fc3113c46c886192e1a385ea7686091368e4e7a
sauloproetti <sauloproetti@gmail.com>
2025-02-06 21:58:37 -0300

    add method apply damage and die animation of player

--- game/enemies/enemy_base.gd
+++ game/enemies/enemy_base.gd
@@ -3,16 +3,19 @@ class_name EnemyBase extends CharacterBody2D
 @export_category("Variables")
 @export var animated_sprite: AnimatedSprite2D
 @export var body_collision: CollisionShape2D
+@export var damage_collision: CollisionShape2D
 @export var speed: float
+@export var damage: int
 
 func _process(_delta: float) -> void:
 	_movement()
 	_animation()
 
 func _movement():
-	var direction = (Global.player.global_position - global_position).normalized()
-	velocity = speed * direction
-	move_and_slide()
+	if Global.player:
+		var direction = (Global.player.global_position - global_position).normalized()
+		velocity = speed * direction
+		move_and_slide()
 
 func _animation():
 	if velocity.x > 0:
@@ -20,3 +23,8 @@ func _animation():
 	else:
 		animated_sprite.flip_h = true
 	animated_sprite.play("idle")
+
+func _on_area_damage_body_entered(body):
+	var player = body as PlayerController
+	if player and Global.player:
+		player._apply_damage(damage)
--- game/player/player.gd
+++ game/player/player.gd
@@ -1,16 +1,36 @@
 class_name PlayerController extends CharacterBody2D
 
+enum PlayerState {Normal, Dead}
+
 const SPEED = 300.0
+const MAX_HEALTH = 100
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
+
+var current_health: int
+
+var current_state: PlayerState:
+	set(new_value):
+		match new_value:
+			PlayerState.Dead:
+				animated_sprite.play("death")
+				current_state = PlayerState.Dead
+				Global.player = null
 
 func _ready():
 	Global.player = self
+	current_health = MAX_HEALTH
+	current_state = PlayerState.Normal
+
+func _process(delta):
+	_animation()
 
 func _physics_process(_delta: float):
 	_moviment()
-	_animation()
 
 func _animation():
+	if current_state == PlayerState.Dead:
+		return
+		
 	if velocity.x > 0:
 		animated_sprite.flip_h = true
 	else:
@@ -19,6 +39,23 @@ func _animation():
 	animated_sprite.play("idle")
 
 func _moviment():
+	if current_state == PlayerState.Dead:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+	
 	var direction = Input.get_vector("ui_left", "ui_right","ui_up", "ui_down")
 	velocity = SPEED * direction
 	move_and_slide()
+
+func _apply_damage(damage: int):
+	if current_state == PlayerState.Dead:
+		return
+	
+	current_health -= damage
+	print(current_health)
+	
+	if current_health <= 0:
+		current_health = 0
+		current_state = PlayerState.Dead
+	
